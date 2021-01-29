d75cd38f6e442853a4fdab44a4c5b66c043ddfc0
DCR430 <daniel.c.reyes30@gmail.com>
2021-01-29 11:47:48 -0500

    header

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div className="header_wrapper">
+            {/* logo */}
+            {/* search */}
+            {/* menu */}
+            
+        </div>
+    )
+}
+
+export default Header
