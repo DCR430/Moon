21d38ef72336b3946e1404ec1e4d5300c16ded18
DCR430 <daniel.c.reyes30@gmail.com>
2021-01-29 16:48:54 -0500

    chart data

--- src/LineGraph.js
+++ src/LineGraph.js
@@ -2,6 +2,19 @@ import React from 'react'
 import {Line} from "react-chartjs-2"
 
 function LineGraph() {
+
+    const data = [
+        {
+            x:10,
+            y:20
+
+        },
+        {
+            x:15,
+            y:10
+
+        }
+    ]
     return (
         <div className="linegraph">
             <Line
@@ -9,11 +22,20 @@ function LineGraph() {
                 datasets:[
                     {
                         type:"line",
-                        data: data
+                        data: data,
+                        backgroundColor:"transparent",
+                        borderColor:"#f0c420",
+                        borderwidth:3,
+                        pointBorderColor:"red",
+                        pointBackgroundColor:"red",
+                        pointHoverBackgroundColor:"#f0c420",
+                        pointHoverBorderColor:"#000000",
+                        pointHoverBorderWidth:4,
+                        pointHoverRadius:6,
                     }
                 ]
             }}
-            type={line}
+            // type={line}
             
             />
             
