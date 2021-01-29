0197de05ee1f167ba92c2da9e758eeabd1067b14
DCR430 <daniel.c.reyes30@gmail.com>
2021-01-29 12:57:21 -0500

    search menu

--- src/Header.js
+++ src/Header.js
@@ -1,15 +1,27 @@
 import React from 'react'
 import Logo from './logo_rocket.png'
+import './Header.css'
 
 function Header() {
     return (
         <div className="header_wrapper">
             {/* logo */}
             <div className="header_logo">
-                <img src={Logo} width={30}/>
+                <img src={Logo} width={35}/>
             </div>
             {/* search */}
+            <div className="header_search">
+                <div className="header_searchContainer">
+                    <input placeholder="Search" type="text"/>
+                </div>
+            </div>
             {/* menu */}
+            <div className="header_menuItems">
+                <a href="#">Portfolio</a>
+                <a href="#">Cash </a>
+                <a href="#">Messages</a>
+                <a href="#">Account</a>
+            </div>
             
         </div>
     )
