0f445e47f55f89b50ad5766361644a8ef9d1284f
DCR430 <daniel.c.reyes30@gmail.com>
2021-01-29 14:35:00 -0500

    body

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Header'
+import Feed from './Feed'
+import Stats from './Stats'
 
 function App() {
   return (
@@ -9,6 +11,10 @@ function App() {
       <Header/>
       </div>
       <div className="Body">
+        <div className="app_container">
+          <Feed/>
+          <Stats/>
+        </div>
 
       </div>
     
---
+++ src/Feed.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Feed.css'
+
+function Feed() {
+    return (
+        <div className="Feed">
+            <div className="feed_container">
+
+            </div>
+        </div>
+    )
+}
+
+export default Feed
