4d8a6362775439e66aac7c051b3b82d93376f16e
DCR430 <daniel.c.reyes30@gmail.com>
2021-01-31 19:19:05 -0500

    firebase

---
+++ src/Firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAoFXW9b3T7yNvE9gG_t6qZ8O7ljx0IK-Y",
+    authDomain: "moon-3c490.firebaseapp.com",
+    projectId: "moon-3c490",
+    storageBucket: "moon-3c490.appspot.com",
+    messagingSenderId: "280633531243",
+    appId: "1:280633531243:web:7a20ddc8b664c23be64a2e"
+  };
